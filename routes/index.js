a44df8f131b839ba2bb0e174cafbc429a435b549
Shinumol Sebastian <shinumolsebas@gmail.com>
2023-06-04 00:54:07 -0300

    partials and contents added

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,18 @@ router.get('/home', function(req, res, next) {
 });
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 /* GET home page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 /* GET home page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 /* GET home page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me' });
 });
 module.exports = router;
